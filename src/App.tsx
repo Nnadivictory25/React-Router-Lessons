d6930fa8452b895815294be4b52a3da316c5a09e
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-30 20:21:04 +0100

    nesting routes, creating pages and layouts for each route

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,19 @@ import Home from './pages/Home';
 
 // Layouts
 import RootLayout from './layouts/RootLayout';
+import HelpLayout from './layouts/HelpLayout';
+import Faq from './pages/help/Faq';
+import Contact from './pages/help/Contact';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<RootLayout />}>
 			<Route path='/' element={<Home />} />
-			<Route path='about' element={<About />} />
+      <Route path='about' element={<About />} />
+      <Route path='help' element={<HelpLayout/>}>
+        <Route path='faq' element={<Faq/>}></Route>
+        <Route path='contact' element={<Contact/>}></Route>
+      </Route>
 		</Route>
 	)
 );
