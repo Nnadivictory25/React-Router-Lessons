22ab9b8bef9fea6797e99689546f9e8a3d405349
Nnadi Victory <chikamagozi25@gmail.com>
2023-04-01 22:26:12 +0100

    use form to fecth data using react-router Form component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import './App.css';
 import About from './pages/About';
 import Home from './pages/Home';
 import Faq from './pages/help/Faq';
-import Contact from './pages/help/Contact';
+import Contact, { contactAction } from './pages/help/Contact';
 import NotFound from './pages/NotFound';
 import Careers, { careersLoader } from './pages/careers/Careers';
 import CareerDetails, {
@@ -32,7 +32,10 @@ const router = createBrowserRouter(
 
 			<Route path='help' element={<HelpLayout />}>
 				<Route path='faq' element={<Faq />}></Route>
-				<Route path='contact' element={<Contact />}></Route>
+				<Route
+					path='contact'
+					element={<Contact />}
+					action={contactAction}></Route>
 			</Route>
 
 			<Route
--- src/pages/help/Contact.tsx
+++ src/pages/help/Contact.tsx
@@ -1,8 +1,12 @@
+import { ActionFunctionArgs, Form, redirect, useActionData } from "react-router-dom";
+
 export default function Contact() {
+  const data = useActionData() as ErrorEvent
+
     return (
       <div className="contact">
         <h3>Contact Us</h3>
-        <form>
+        <Form method="post" action="/help/contact">
           <label>
             <span>Your email:</span>
             <input type="email" name="email" required />
@@ -12,7 +16,29 @@ export default function Contact() {
             <textarea name="message" required></textarea>
           </label>
           <button>Submit</button>
-        </form>
+
+          {data && data.error && <p className="text-red-500">{data.error}</p>}
+        </Form>
       </div>
     )
+}
+  
+export const contactAction = async ({ request }: ActionFunctionArgs) => {
+
+  const data = await request.formData()
+
+  const submission = {
+    email: data.get('email') as string,
+    message: data.get('message') as string
   }
+
+  console.log(submission)
+
+  // validate submission
+  if (submission.message?.length < 10) {
+    return {error: 'Message must be over 10 chars long'}
+  }
+
+  // redirect
+  return redirect('/')
+}
